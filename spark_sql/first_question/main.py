5ca82bdc9ad58d96dbec19f929f88d0cb6b4b8af
Antonio Della Porta <antodp00@gmail.com>
2024-05-16 18:06:36 +0200

    First questiion with sparkSQL

---
+++ spark_sql/first_question/main.py
@@ -0,0 +1,48 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum, max
+from pyspark.sql.functions import lag, col
+from pyspark.sql.window import Window
+
+
+import argparse
+
+# create parser and set its arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_path", type=str, help="Input file path")
+
+# parse arguments
+args = parser.parse_args()
+input_filepath = args.input_path
+
+
+# Inizializza SparkSession
+spark = SparkSession.builder \
+    .appName("Stock Statistics") \
+    .getOrCreate()
+
+merged_df = spark.read.csv(input_filepath, header = True)
+
+merged_df.show()
+
+# Registra i DataFrame come tabelle temporanee per l'utilizzo di SparkSQL
+merged_df.createOrReplaceTempView("merged_table")
+
+# Calcola le statistiche richieste per ciascuna azione e anno
+stock_statistics_df = spark.sql("""
+    SELECT ticker,
+           name,
+           year,
+           ROUND((LAST(close) - FIRST(close)) / FIRST(close) * 100, 2) AS percent_change,
+           MIN(low) AS min_price,
+           MAX(high) AS max_price,
+           AVG(volume) AS avg_volume
+    FROM merged_table
+    GROUP BY ticker, name, year
+    ORDER BY ticker, year
+""")
+
+# Mostra il DataFrame con le statistiche
+stock_statistics_df.show()
+
+# Chiudi la sessione Spark
+spark.stop()
