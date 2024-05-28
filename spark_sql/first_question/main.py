446705df744ec75c28933a7d77740678ea3b82c2
Antonio Della Porta <antodp00@gmail.com>
2024-05-28 17:56:52 +0200

    Update main.py

--- spark_sql/first_question/main.py
+++ spark_sql/first_question/main.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import sum, max
 from pyspark.sql.functions import lag, col
 from pyspark.sql.window import Window
 import time
-
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
 import argparse
 
 
@@ -22,8 +22,24 @@ spark = SparkSession.builder \
     .appName("Stock Statistics") \
     .getOrCreate()
 
+schema = StructType([
+    StructField("ticker", StringType(), True),
+    StructField("open_price", DoubleType(), True),
+    StructField("close", DoubleType(), True),
+    StructField("low", DoubleType(), True),
+    StructField("high", DoubleType(), True),
+    StructField("volume", IntegerType(), True),
+    StructField("date", StringType(), True),
+    StructField("year", IntegerType(), True),
+    StructField("exchange", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("sector", StringType(), True),
+    StructField("industry", StringType(), True)
+])
 
-merged_df = spark.read.csv(input_filepath, header = True)
+# Leggere il file CSV con lo schema definito
+merged_df = spark.read.option("delimiter", ";").csv(input_filepath, header=True, schema=schema)
+
 merged_df = merged_df.orderBy("ticker", "date")
 
 merged_df.show(truncate = False)
@@ -59,7 +75,7 @@ stock_statistics_df = spark.sql("""
 """)
 
 # Mostra il DataFrame con le statistiche
-stock_statistics_df.show()
+# stock_statistics_df.show()
 stock_statistics_df.createOrReplaceTempView("stock_yearly_stats")
 
 result = spark.sql(
@@ -80,11 +96,11 @@ result = spark.sql(
 )
 result.show()
 
-# result.write \
-#     .format("csv") \
-#     .mode("overwrite") \
-#     .option("header", "true") \
-#     .save("file:///home/addi/bigData/secondo_progetto/Big_Data_Second_Project/spark_sql/first_question/csv_file")
+result.write \
+    .format("csv") \
+    .mode("overwrite") \
+    .option("header", "true") \
+    .save("file:///home/addi/bigData/secondo_progetto/Big_Data_Second_Project/spark_sql/first_question/csv_file")
 # Chiudi la sessione Spark
 spark.stop()
 
